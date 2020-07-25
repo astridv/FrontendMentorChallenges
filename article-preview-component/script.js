6b942adb28c85a1b723a113ef1331718dfa8fbcb
Astrid Vik <astridvi@stud.ntnu.no>
2020-07-25 21:21:02 +0200

    Implement functionality for toggling social icons, make minor alignment tweaks

---
+++ article-preview-component/script.js
@@ -0,0 +1,14 @@
+function showSocials() {
+  console.log("Click");
+  let contributorContainer = document.getElementById("contributor-container");
+  let shareContainer = document.getElementById("share-container");
+  contributorContainer.style.display = "none";
+  shareContainer.style.display = "flex";
+}
+
+function hideSocials() {
+  let shareContainer = document.getElementById("share-container");
+  let contributorContainer = document.getElementById("contributor-container");
+  shareContainer.style.display = "none";
+  contributorContainer.style.display = "flex";
+}
