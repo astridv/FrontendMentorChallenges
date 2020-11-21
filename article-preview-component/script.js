69a719c38e62376f9b8e2c25cdec5058fd923c92
Astrid Vik <astridvi@stud.ntnu.no>
2020-11-21 19:24:14 +0100

    Fix and simplify logic for toggling social media icons

--- article-preview-component/script.js
+++ article-preview-component/script.js
@@ -1,44 +1,7 @@
 let shareContainer = document.getElementById("share-container");
-let contributorContainer = document.getElementById("contributor-container");
-let showSocialsButton = document.getElementById("hide-socials-button");
-let shareIcon = document.getElementById("share-icon");
+let showSocialsButton = document.getElementById("show-socials-button");
 
-
-function toggleSocials() {
-  console.log("showSocials method fired");
-  if (getWidth() < 600) {
-    contributorContainer.style.display = "none";
-    shareContainer.style.display = "flex";
-  } else {
-    let displaySetting = shareContainer.style.display;
-    /* if shareContainer is not visible */
-    if (displaySetting == "none") {
-      showSocialsButton.style.display = "none";
-      shareContainer.style.display = "flex";
-    }
-    else {
-      showSocialsButton.style.display = "flex";
-      shareContainer.style.display = "none";
-    }
-    
-  }
+function toggleSocials() {  
+  shareContainer.style.display = shareContainer.style.display == "flex" ? "none" : "flex";
+  showSocialsButton.classList.toggle("active-icon");
 }
-
-function hideSocialsOnMobile() {
-  shareContainer.style.display = "none";
-  contributorContainer.style.display = "flex";
-}
-
-function getWidth() {
-  xWidth = null;
-  if (window.screen != null) xWidth = window.screen.availWidth;
-
-  if (window.innerWidth != null) xWidth = window.innerWidth;
-
-  if (document.body != null) xWidth = document.body.clientWidth;
-  return xWidth;
-}
-
-
-
-
