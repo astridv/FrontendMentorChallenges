41626f3320c6db4456810b5eadc75a6dd57e5ba9
Astrid Vik <astridvi@stud.ntnu.no>
2021-03-27 20:04:47 +0100

    Add logic for discount when yearly billing

--- interactive-pricing-component/script.js
+++ interactive-pricing-component/script.js
@@ -3,10 +3,16 @@ let pageviewsNum = document.getElementById('pageviewsNumber');
 let priceElement = document.getElementById('price');
 let pageviews;
 let price;
+let discount = 1;
 
 // set intial values of slider
 priceElement.innerHTML = 16;
 pageviewsNum.innerHTML = '100K';
+
+function handleClick(cb) {
+    discount = (cb.checked) ? 0.75 : 1;
+    updateViewsAndPrice(slider.value);
+}
 
 slider.oninput = function() {
   /* handle fill color in chrome */
@@ -17,26 +23,25 @@ slider.oninput = function() {
 }
 
 function updateViewsAndPrice(views) {
-    console.log(views);
     switch(+views) {
         case 1:
-            price = 8;
+            price = 8 * discount;
             pageviews = '10K';
             break;
         case 2:
-            price = 12;
+            price = 12 * discount;
             pageviews = '50K';
             break;
         case 3:
-            price = 16;
+            price = 16 * discount;
             pageviews = '100K';
             break;
         case 4:
-            price = 24;
+            price = 24 * discount;
             pageviews = '500K';
             break;
         case 5:
-            price = 36;
+            price = 36 * discount;
             pageviews = '1M';
             break;
     }
